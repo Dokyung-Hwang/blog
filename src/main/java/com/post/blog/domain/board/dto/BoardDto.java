8cda393ebd30c4bf2ea4d93b6f9985c6f9922555
Smile:DK <dokyung226@gmail.com>
2025-03-02 14:54:24 +0900

    ✨Feat&Refactor: 댓글 기능 로직 수정 및 게시판 상세 조회 시 댓글 리스트 추가, 게시판 페이지 조회 추가

--- src/main/java/com/post/blog/domain/board/dto/BoardDto.java
+++ src/main/java/com/post/blog/domain/board/dto/BoardDto.java
@@ -2,11 +2,17 @@ package com.post.blog.domain.board.dto;
 
 import com.post.blog.domain.account.entity.Account;
 import com.post.blog.domain.board.entity.Board;
+import com.post.blog.domain.comment.dto.CommentDto;
+import com.post.blog.domain.comment.entity.Comment;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class BoardDto {
@@ -51,15 +57,33 @@ public class BoardDto {
 
     @Getter
     public static class Response {
+        // Boards
         private final Long boardId;
         private final String title;
         private final String content;
+        private List<CommentDto.Response> comments;
 
         @Builder
         public Response(Long boardId, String title, String content) {
             this.boardId = boardId;
             this.title = title;
             this.content = content;
+        }
+
+        @Builder
+        public Response(Long boardId, String title, String content, List<CommentDto.Response> comments) {
+            this.boardId = boardId;
+            this.title = title;
+            this.content = content;
+            this.comments = comments;
+        }
+
+        public static Response from(Board board) {
+            List<CommentDto.Response> commentDtoList = board.getComments().stream()
+                    .map(CommentDto.Response::from)
+                    .collect(Collectors.toList());
+
+            return new Response(board.getBoardId(), board.getTitle(), board.getContent(), commentDtoList);
         }
 
     }
